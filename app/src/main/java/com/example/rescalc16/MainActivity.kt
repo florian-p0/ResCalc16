e1dd668747e7390cd5460a980bb0e95a9a526753
Florian Paesler <florian.paesler@student.uni-siegen.de>
2024-04-16 18:47:58 +0200

    added simple widget for testing purposes

--- app/src/main/java/com/example/rescalc16/MainActivity.kt
+++ app/src/main/java/com/example/rescalc16/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rescalc16
 
 import android.os.Bundle
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,29 +16,6 @@ import com.example.rescalc16.ui.theme.ResCalc16Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ResCalc16Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ResCalc16Theme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
